a0a164f8e6bea126b6d6dd4319d91e00c8140ebf
Arzhancev Michael TV-11 <104345619+Akella11@users.noreply.github.com>
2024-06-08 11:09:00 +0300

    Create data_types.py

---
+++ data_types.py
@@ -0,0 +1,20 @@
+# 1. Надайте змінним значення
+varInt = 10
+varFloat = 8.4
+varStr = "No"
+
+# 2. Збільшіть значення varInt у 3.5 рази
+varBig = varInt * 3.5
+
+# 3. Зменшіть значення varFloat на одиницю
+varFloat -= 1
+
+# 4. Розділіть varInt на varFloat, потім varBig на varFloat
+print(varInt / varFloat)
+print(varBig / varFloat)
+
+# 5. Змініть значення varStr на "NoNoYesYesYes"
+varStr = "No" * 2 + "Yes" * 3
+
+# 6. Виведіть значення всіх змінних
+print(f"varInt: {varInt}, varFloat: {varFloat}, varBig: {varBig}, varStr: {varStr}")
