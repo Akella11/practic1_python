f533389713ecc5f0642a1ea5faa3ad7f5f8254c1
Arzhancev Michael TV-11 <104345619+Akella11@users.noreply.github.com>
2024-06-08 11:08:15 +0300

    Create user.py

---
+++ user.py
@@ -0,0 +1,9 @@
+# Запитуємо у користувача дані
+name = input("What is your name? ")
+age = input("How old are you? ")
+location = input("Where do you live? ")
+
+# Виводимо отримані дані
+print(f"This is {name}")
+print(f"It is {age}")
+print(f"(S)he lives in {location}")
