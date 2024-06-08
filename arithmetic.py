10575a311c04df42602032aa695f9095e2af863e
Arzhancev Michael TV-11 <104345619+Akella11@users.noreply.github.com>
2024-06-08 11:07:49 +0300

    Create arithmetic.py

---
+++ arithmetic.py
@@ -0,0 +1,9 @@
+# Запропонуйте користувачеві вирішити приклад
+user_answer = input("Solve the example 4 * 100 - 54: ")
+
+# Обчислюємо правильну відповідь
+correct_answer = 4 * 100 - 54
+
+# Виводимо правильну відповідь і відповідь користувача
+print(f"Correct answer: {correct_answer}")
+print(f"Your answer: {user_answer}")
